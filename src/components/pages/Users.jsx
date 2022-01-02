5dc1b72883641d75f33913eb66dc5ee9f64b57af
kei86 <61891585+kei86@users.noreply.github.com>
2022-01-02 16:52:29 +0900

    OK?

--- src/components/pages/Users.jsx
+++ src/components/pages/Users.jsx
@@ -2,6 +2,9 @@ import styled from "styled-components";
 import { SearchInput } from "../molecules/SearchInput";
 import { UserCard } from "../organisms/user/UserCard";
 import { useLocation } from "react-router-dom";
+import { SecondaryButton } from "../atoms/button/SecondaryButton";
+import { useContext } from "react";
+import { UserContext } from "../../providers/UserProvider";
 const users = [...Array(10).keys()].map((val) => {
   return {
     id: val,
@@ -19,13 +22,14 @@ const users = [...Array(10).keys()].map((val) => {
 const user = {};
 
 export const Users = () => {
-  const { state } = useLocation();
-  const isAdmin = state ? state.isAdmin : false;
-  console.log(state);
+  const { userInfo, setUserInfo } = useContext(UserContext);
+  const onClickSwitch = () => setUserInfo({ isAdmin: !userInfo.isAdmin });
   return (
     <SContainer>
       <h2>User page </h2>
       <SearchInput />
+      <br />
+      <SecondaryButton onClick={onClickSwitch}>切り替え</SecondaryButton>
       <SUserArea>
         {users.map((user) => (
           <UserCard key={user.id} user={user} />
